1a2e17be6f80cd9d4c9776bee5ea0e98d1170d17
qhai-dev <qhai.dev@gmail.com>
2026-06-30 21:56:30 +0800

    todo

--- backend/app/admin/manager/cmd/main.go
+++ backend/app/admin/manager/cmd/main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/qhai-dev/kairo/backend/library/framework"
 )
 
 func main() {
 	fmt.Printf("hello bazel build remote test")
-	app := framework.New()
-	app.Run()
 }
